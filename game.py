d2287d068dff6cd4a77ea391a253782e23850ed2
MRpeackle <64694816+MRpeackle@users.noreply.github.com>
2024-02-15 16:47:03 +0200

    Create game.py

---
+++ game.py
@@ -0,0 +1,34 @@
+import random
+
+repeat = True
+
+while repeat:
+    number = random.randint(1, 100)
+    guess = 0
+    tries = 0
+
+    while guess != number:
+        if guess < number:
+            print("lower. ")
+        else:
+            print("higher. ")
+        
+        guess = int(input("guess the number: "))
+        tries += 1
+    else:
+        if tries < 4:
+            print(f"wow it only took you {tries} treis")
+        elif tries < 7:
+            print(f"not bad but it took you a whole {tries} tries")
+        else:
+            print(f"damn it took you {tries} tries to guess you shit at this")
+
+    response = input("do you wish to continue? y/n: ")
+    if response == "y":
+        repeat = True
+    elif response == "n":
+        repeat = False
+        print("thank you for playing bye bye")
+    else:
+        repeat = False
+        print("thank you for playing bye bye")
